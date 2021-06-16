4b2e88a37888a0f93b5177fa59ce4ad7592f0fac
Kristian Matthews <kristian.matthews@me.com>
2021-06-16 14:19:38 +0100

    Move map script

--- assets/js/starter.js
+++ assets/js/starter.js
@@ -20,8 +20,6 @@ import "../../node_modules/bootstrap/js/dist/base-component.js";
 import "../../node_modules/bootstrap/js/dist/collapse.js";
 import "../../node_modules/bootstrap/js/dist/scrollspy.js";
 
-const tokenId = "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlRWWTJZU0hUMk0ifQ.eyJpc3MiOiIzNFdVS1FCWFhIIiwiaWF0IjoxNjIzODQ4MzEwLCJleHAiOjE2NTUzMzc2MDAsIm9yaWdpbiI6IndlZGRpbmcubWF0dGhld3Mta2VubmluZ3Rvbi5jb20ifQ.WqERpuk9Ol5SZsEodrDas4FLRItWD9U1DWxIHwK14Beq3IYM6-p0JXDtoYX5xIth6xTGfW5YQBPvk4OmYR8Yjw";
-
 var heroVideo = document.getElementById("hero-video");
 
 var heroVideoPromise = heroVideo.play();
@@ -31,57 +29,3 @@ if (heroVideoPromise !== undefined) {
     heroVideo.remove();
   });
 }
-
-mapkit.init({
-  authorizationCallback: function(done) {
-      done(tokenID);
-  }
-});
-
-var map = new mapkit.Map("map");
-
-var crownePlaza = new mapkit.CoordinateRegion(
-  new mapkit.Coordinate(53.796570, -1.557678)
-);
-
-var busStation = new mapkit.CoordinateRegion(
-  new mapkit.Coordinate(53.797183, -1.535897)
-);
-
-var trainStation = new mapkit.CoordinateRegion(
-  new mapkit.Coordinate(53.794379, -1.548459)
-);
-
-var airport = new mapkit.CoordinateRegion(
-  new mapkit.Coordinate(53.869003, -1.659751)
-);
-
-var crownePlazaAnnotation = new MarkerAnnotation(crownePlaza, {
-  color: "#202e52",
-  title: "Crowne Plaza",
-  subtitle: "Leeds",
-  selected: true
-});
-
-var busStationAnnotation = new MarkerAnnotation(busStation, {
-  title: "Bus Station",
-  subtitle: "Leeds"
-});
-
-var trainStationAnnotation = new MarkerAnnotation(trainStation, {
-  title: "Train Station",
-  subtitle: "Leeds"
-});
-
-var airportAnnotation = new MarkerAnnotation(airport, {
-  title: "Airport",
-  subtitle: "Leeds Bradford LBA"
-});
-
-map.region = crownePlaza;
-map.showItems([
-  crownePlazaAnnotation,
-  busStationAnnotation,
-  trainStationAnnotation,
-  airportAnnotation
-]);
